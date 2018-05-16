817dbf81dcb10ff52b01131082cf45a53ff41085
Lyzzz <947652963@qq.com>
2018-05-16 17:50:54 +0800

    '更新M'

--- myblog/settings.py
+++ myblog/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
         'USER': 'root',
-        'PASSWORD': 'asd123456',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
